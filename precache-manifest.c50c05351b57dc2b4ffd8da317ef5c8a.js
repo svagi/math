ea2e9c01ab38095ee9bce10ccf7cf76bc6ed8a63
jan.svager <jan.svager@workday.com>
2020-01-12 22:26:02 +0100

    Updates

---
+++ precache-manifest.c50c05351b57dc2b4ffd8da317ef5c8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23333fd81a7d28660559de2cf06f4297",
+    "url": "/math/index.html"
+  },
+  {
+    "revision": "54c5b55b86d7510b9062",
+    "url": "/math/static/js/2.d862486f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/math/static/js/2.d862486f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0449c70dccde168bd514",
+    "url": "/math/static/js/main.6d9512a4.chunk.js"
+  },
+  {
+    "revision": "992c6d6d7c034b1a77c1",
+    "url": "/math/static/js/runtime-main.f538d2f6.js"
+  }
+]);
